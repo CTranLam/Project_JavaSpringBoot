3475511440cf44062c3577ccb91b392aced2061d
tlam15282@gmail.com <tlam15282@gmail.com>
2025-07-24 09:38:02 +0700

    update

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -32,7 +32,7 @@ public class BuildingAPI {
 	@PostMapping("/api/building/")
 	public List<BuildingDTO> getBuilding(@RequestParam(value = "name" ,required = false)String name,
 										@RequestParam(value = "districtId" ,required = false)Long district,
-										@RequestParam(value = "typeCode" ,required = false) List<String>typecode){
+										@RequestParam(value = "typeCode" ,required = false) List<String>typeCode){
 		List<BuildingDTO> result = buildingService.findAll(name,district);
 		return result;
 	}
